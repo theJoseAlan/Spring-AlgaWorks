5b244984658567cc34acc84ab33e510eda98028b
theJoseAlan <117518719+theJoseAlan@users.noreply.github.com>
2023-01-09 17:05:36 -0300

    Método para exclusão

--- src/main/java/com/algaworks/algalog/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/controller/ClienteController.java
@@ -6,10 +6,9 @@ import jakarta.persistence.EntityManager;
 import lombok.AllArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import jakarta.persistence.PersistenceContext;
 import java.util.List;
@@ -17,6 +16,7 @@ import java.util.Optional;
 
 @AllArgsConstructor //Gera os construtores
 @RestController
+@RequestMapping("/clientes") //Assim não preciso colocar "/clientes" em all GetMapping
 public class ClienteController {
 
     /*@PersistenceContext
@@ -25,7 +25,7 @@ public class ClienteController {
     //@Autowired //Injetar uma instancia gerenciada pelo Spring
     private ClienteRepository clienteRepository;
 
-    @GetMapping("/clientes")
+    @GetMapping
     public List<Cliente> listar(){
 
         return clienteRepository.findAll();
@@ -48,18 +48,55 @@ public class ClienteController {
 
     }*/
 
-    @GetMapping("/clientes/{clienteId}")
+    @GetMapping("/{clienteId}")
     public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId){
-        Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+
+        return clienteRepository.findById(clienteId)
+                .map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+
+        //ou
+
+        /*Optional<Cliente> cliente = clienteRepository.findById(clienteId);
 
         if(cliente.isPresent()){ //se tiver alguma coisa
             return ResponseEntity.ok(cliente.get()); //retorna o que tem dentro do cliente
         }
 
-        return ResponseEntity.notFound().build(); //Not found se nao encontrar nada (retorna um 404)
+        return ResponseEntity.notFound().build(); //Not found se nao encontrar nada (retorna um 404)*/
 
         //return cliente.orElse(null); //Se não tiver nada ele retorna null (apenas se usa 'Cliente'
         // ao invés do ReponseEntity<Cliente>
+    }
+
+    //Inserindo cliente
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@RequestBody Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
+    //Atualizando Cliente
+    @PutMapping("/{clienteId}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @RequestBody Cliente cliente){
+        if(!clienteRepository.existsById(clienteId)){//Se o cliente não existir
+            return ResponseEntity.notFound().build();
+        }
+        cliente.setId(clienteId);
+        cliente = clienteRepository.save(cliente);
+
+        return ResponseEntity.ok(cliente);
+    }
+
+    //Deletando clientes
+    @DeleteMapping("/{clienteId}")
+    public ResponseEntity<Void> remover(@PathVariable Long clienteId){
+        if(!clienteRepository.existsById(clienteId)){
+            return ResponseEntity.notFound().build(); //retorna 404, se nao existir
+        }
+
+        clienteRepository.deleteById(clienteId);
+
+        return ResponseEntity.noContent().build(); //Retorna o 204
     }
 
 
